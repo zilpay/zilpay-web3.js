f7e613b7f67b574383c8e3cf1f69eb937b757250
Rinat <lich666black@gmail.com>
2021-12-28 15:13:33 +0300

    fixed assert

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import { swc } from 'rollup-plugin-swc3';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
+import define from 'rollup-plugin-define';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -23,20 +25,14 @@ export default {
 			brower: true,
 			preferBuiltins: false
 		}),
-		// production && visualizer({
-		// 	filename: 'inpage.html'
-		// }),
-		// production && terser({
-		// 	format: {
-		// 		comments: false
-		// 	},
-		// 	compress: true
-		// }),
-		// define({
-    //   replacements: {
-		// 		WebAssembly: 'null'
-    //   }
-    // }),
+		production && visualizer({
+			filename: 'inpage.html'
+		}),
+		define({
+      replacements: {
+				WebAssembly: 'null'
+      }
+    }),
     swc({
       // All options are optional
       include: /\.[jt]sx?$/, // default
